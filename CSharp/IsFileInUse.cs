e49a82ac8ce0a0a1417ca0ba97f02e00d93eb569
Van Eester Michiel <michielvaneester@gmail.com>
2021-11-12 14:13:16 +0100

    Class IsFileLocked

---
+++ CSharp/IsFileInUse.cs
@@ -0,0 +1,25 @@
+//This code snippet is a  function to check if a file you want to import is already used by another process.
+
+//Main code
+FileInfo file = new FileInfo(dialog.FileName);
+if (!IsFileLocked(file)) {
+    //Code here
+}
+else {
+    //File is used by another Process
+}
+
+//Class
+protected virtual bool IsFileLocked(FileInfo file)
+{
+    try {
+        using (FileStream stream = file.Open(Filemode.Open, FileAccess.Read, FileShare.None)) {
+            stream.Close();
+        }
+    }
+    catch (Exception ex) {
+        return true;
+    }
+
+    return false;
+}
