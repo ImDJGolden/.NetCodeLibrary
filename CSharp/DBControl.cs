bc7ac2455d64473288e4bd82e57755d44c237c35
M Van Eester <43848511+ImDJGolden@users.noreply.github.com>
2021-11-17 16:11:28 +0100

    Update DBControl.cs
    DBControl for access database added

--- CSharp/DBControl.cs
+++ CSharp/DBControl.cs
@@ -101,3 +101,102 @@ public bool UpdateDataTable(string sql)
         cnn.Close();
     }
 }
+
+
+//ACCESS DATABASE:
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+
+namespace DBControl
+{
+    public class DBKlassement
+    {
+        public static string mCS = System.Configuration.ConfigurationManager.ConnectionStrings["Vanlommel.Properties.Settings.ScanVettenDbConnectionString"].ConnectionString;
+
+        public OleDbConnection OpenConnection()
+        {
+            OleDbConnection cnn = new OleDbConnection(mCS);
+
+            try
+            {
+                cnn.Open();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return cnn;
+        }
+
+        public bool CloseConnection(OleDbConnection cnn)
+        {
+            try
+            {
+                cnn.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return true;
+        }
+
+        public DataTable GetDataTable(string sql)
+        {
+            OleDbConnection cnn = new OleDbConnection(mCS);
+            OleDbCommand cmd = new OleDbCommand(sql, cnn);
+            DataTable dt = new DataTable();
+
+            try
+            {
+                cnn.Open();
+                OleDbDataAdapter oda = new OleDbDataAdapter(cmd);
+                oda.Fill(dt);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                cnn.Close();
+            }
+            return dt;
+        }
+
+        public bool UpdateDataTable(string sql)
+        {
+            OleDbConnection cnn = new OleDbConnection(mCS);
+            OleDbCommand cmd = new OleDbCommand(sql, cnn);
+            int result;
+
+            try
+            {
+                cnn.Open();
+                result = cmd.ExecuteNonQuery();
+
+                if (result < 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                cnn.Close();
+            }
+        }
+    }
+}
