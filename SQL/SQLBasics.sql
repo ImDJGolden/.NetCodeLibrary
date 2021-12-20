09fc5d8326e2d7978bfb115af97370278869878c
Van Eester Michiel <michielvaneester@gmail.com>
2021-12-20 13:27:02 +0100

    SQL Math added

--- SQL/SQLBasics.sql
+++ SQL/SQLBasics.sql
@@ -1,17 +1,17 @@
 -- *** Basic Syntax
 
-SELECT;                                     -- Haal data van DB
-UPDATE;                                     -- Update data van DB
-DELETE;                                     -- Delete data van DB
-INSERT INTO;                                -- Voeg nieuwe data to aan DB
+SELECT;                                     -- Haal data van DB.
+UPDATE;                                     -- Update data van DB.
+DELETE;                                     -- Delete data van DB.
+INSERT INTO;                                -- Voeg nieuwe data to aan DB.
 
-WHERE;                                      -- Filter records
+WHERE;                                      -- Filter records.
 AND, OR, NOT;                               -- WHERE kan gecombineerd worden met deze.
-ORDER BY;   ASC|DESC                        -- Sorteer resultaat ASC of DESC
+ORDER BY;   ASC|DESC                        -- Sorteer resultaat ASC of DESC.
 
 
 -- *** SELECT
-SELECT <column> FROM <tabel>
+SELECT <kolom> FROM <tabel>
 WHERE <conditie>;
 --AND | OR | NOT <conditie>
 --ORDER BY <tabel> ASC | DESC
@@ -19,17 +19,17 @@ WHERE <conditie>;
 
 -- *** UPDATE
 UPDATE <tabel>
-SET <column> = <waarde>, ...
+SET <kolom> = <waarde>, ...
 WHERE <conditie>;
 
 
 -- *** INSERT INTO
-INSERT INTO <tabel> (<column1>, <column2>, ...)         -- Specifieer zowel columnen en waardes
+INSERT INTO <tabel> (<kolom1>, <kolom2>, ...)         -- Specifieer zowel kolommen en waardes
 VALUES (<waarde1>, <waarde2>, ...);
 
 -- Or
 
-INSERT INTO <tabel>                                     -- Voeg waardes toe aan alle columnen
+INSERT INTO <tabel>                                     -- Voeg waardes toe aan alle kolommen
 VALUES (<waarde1>, <waarde2>, ...);
 
 
---
+++ SQL/SQLMath.sql
@@ -0,0 +1,36 @@
+-- *** Wiskundige functies
+
+MIN() | MAX();                          -- Haal het kleinste of grootste getal uit een kolom.
+COUNT();                                -- Geeft aantal rijen terug die voldoen aan conditie.
+AVG();                                  -- Geeft het gemiddelde van een numerieke kolom.
+SUM();                                  -- Geeft de totale som van een numerieke kolom.
+
+
+-- *** MIN() | MAX()
+SELECT MIN(<kolom>)                         -- Minimum waarde
+FROM <tabel>;
+--WHERE <condition>
+
+-- Or
+
+SELECT MAX(<kolom>)                         -- Maximum waarde
+FROM <tabel>;
+--WHERE <conditie>
+
+
+-- *** COUNT()
+SELECT COUNT(<kolom>)
+FROM <tabel>
+--WHERE <conditie>
+
+
+-- *** AVG()
+SELECT AVG(<kolom>)
+FROM <tabel>
+--WHERE <conditie>
+
+
+-- *** SUM()
+SELECT SUM(<kolom>)
+FROM <tabel>
+--WHERE <conditie>
