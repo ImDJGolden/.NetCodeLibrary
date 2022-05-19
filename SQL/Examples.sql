f0ea1c096715ddfbda62d2bd735dde44af01d7dd
Van Eester Michiel <michielvaneester@gmail.com>
2022-05-19 09:13:23 +0200

    Update Examples.sql

--- SQL/Examples.sql
+++ SQL/Examples.sql
@@ -2,4 +2,18 @@
 SELECT * FROM VK_PrijsToeslagen 
 WHERE vkptKlantklasse = 1;
 
+-- Select a single cell from table where ...
+SELECT csbGLN FROM CSBAdressen
+WHERE csbUBN = '1005698'
+
+-- Update cells from table where ...
+UPDATE Klassementen
+SET klaKlassement = 1, klaRichtPrijs = 200, klaSync = false, klaUpdateDatum = '01/01/2000'
+WHERE klaOornummer = 'BE45672138'
+
+-- Delete records from table where ...
+DELETE 
+FROM Klassementen
+WHERE klaOornummer = 'BE10035689'
+
 -- ...
