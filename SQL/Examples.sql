d6ecc31884808e8dd2a8117366386712cb2bf490
Van Eester Michiel <michielvaneester@gmail.com>
2022-05-30 11:01:35 +0200

    Update Examples.sql

--- SQL/Examples.sql
+++ SQL/Examples.sql
@@ -4,16 +4,20 @@ WHERE vkptKlantklasse = 1;
 
 -- Select a single cell from table where ...
 SELECT csbGLN FROM CSBAdressen
-WHERE csbUBN = '1005698'
+WHERE csbUBN = '1005698';
+
+-- Insert new row into table ...
+INSERT INTO ScanVetten (svDatum, svMesterID, svOornummer, svOornummerMCC, svBeslag, svDatumInScanPaspoort)
+VALUES ('2022/01/01', 'mesterID', 'BE12345683', 'BE945681297', 'beslagIn', '2022/01/01');
 
 -- Update cells from table where ...
 UPDATE Klassementen
 SET klaKlassement = 1, klaRichtPrijs = 200, klaSync = false, klaUpdateDatum = '01/01/2000'
-WHERE klaOornummer = 'BE45672138'
+WHERE klaOornummer = 'BE45672138';
 
 -- Delete records from table where ...
 DELETE 
 FROM Klassementen
-WHERE klaOornummer = 'BE10035689'
+WHERE klaOornummer = 'BE10035689';
 
 -- ...
