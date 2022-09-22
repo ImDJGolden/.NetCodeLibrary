5ee657e58f7adcec33a38e089e22c7f6672cfc08
Van Eester Michiel <michielvaneester@gmail.com>
2022-09-22 06:53:09 +0200

    updated vb library

---
+++ Visual Basic/DgvImageColumn.vb
@@ -0,0 +1,28 @@
+Private Sub FormSelectContainer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Try
+        dtLeeggoed = dblg.GetLeeggoed()
+        dtLeeggoed.Columns.Add("Img", GetType(Bitmap))
+
+        For i As Integer = 0 To dtLeeggoed.Rows.Count - 1
+            Dim img As Image = GetImg(dtLeeggoed(i)("lgcArtikelPicture"))
+            img = New Bitmap(img, New Size(60, 60))
+
+            dtLeeggoed.Rows(i)("Img") = img
+        Next
+
+        Me.dgvContainers.DataSource = dtLeeggoed
+    Catch ex As Exception
+        MessageBox.Show($"{ex.Message}", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error)
+    End Try
+End Sub
+
+
+
+Public Function GetImg(ByVal imgName As String) As Image
+    Dim img As String = $"img\{imgName}.bmp"
+    Dim imgPath As String = Application.StartupPath & "\" & img
+
+    Dim imgFile As Image = Image.FromFile(imgPath)
+
+    Return imgFile
+    End Function
