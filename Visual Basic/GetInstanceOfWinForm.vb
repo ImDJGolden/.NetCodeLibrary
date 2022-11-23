4c7c4375bb4e1313f939402278cd53004d64a861
Van Eester Michiel <michielvaneester@gmail.com>
2022-11-23 11:10:16 +0100

    Create GetInstanceOfWinForm.vb

---
+++ Visual Basic/GetInstanceOfWinForm.vb
@@ -0,0 +1,22 @@
+Public Shared myForm As FormVerIngavePrijzenSNIN_Nini = Nothing
+
+    Public Shared Function GetInstance() As FormVerIngavePrijzenSNIN_Nini
+        If myForm Is Nothing Then
+            myForm = New FormVerIngavePrijzenSNIN_Nini
+        End If
+        myForm.BringToFront()
+        Return myForm
+    End Function
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+        myForm = Nothing
+    End Sub
