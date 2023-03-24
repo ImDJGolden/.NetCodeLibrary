234a030f3cf445dd2e5bd827198adae0be78bef3
ImDJGolden <michielvaneester@gmail.com>
2023-03-24 08:11:50 +0100

    VB.Net Code layout

---
+++ Visual Basic/_VisualBasicCodeLayout.vb
@@ -0,0 +1,31 @@
+'IMPORTS
+
+Namespace name
+public Class FormName
+
+#Region "Variables"
+'Region for all Variables
+#End Region
+
+#Region "Construction"
+'Region for construction of the form / form generated code
+#End Region
+
+#Region "KeyPress"
+'Region for keypress handling
+
+#Region "F Functions"
+'Region for function for keypress
+#End Region
+#End Region
+
+#Region "FormEvents"
+'Region for events from all form components
+#End Region
+
+#Region "Functions"
+'Region for functions
+#End Region
+
+End Class
+End Namespace
