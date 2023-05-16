e053a57c35a45e16637bb35b9ccdada90b9b51e2
ImDJGolden <michielvaneester@gmail.com>
2023-05-16 10:47:58 +0200

    Create ExtensionMethods.vb

---
+++ _ClassesToUse/ExtensionMethods.vb
@@ -0,0 +1,10 @@
+Imports System.Reflection
+
+Public Class ExtensionMethods
+    'Makes DGV faster
+	Public Shared Sub DoubleBuffered(ByVal dgv As DataGridView, ByVal setting As Boolean)
+		Dim dgvType As Type = dgv.[GetType]()
+		Dim pi As PropertyInfo = dgvType.GetProperty("DoubleBuffered", BindingFlags.Instance Or BindingFlags.NonPublic)
+		pi.SetValue(dgv, setting, Nothing)
+	End Sub
+End Class
